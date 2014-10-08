d710b0bf46efeaee6b96e07d879b3940d11895d4
nopilas <nopilas@nopilass-Mac.local>
2014-10-08 09:22:09 -0700

    a

---
+++ eLog/UpgraveViewController.h
@@ -0,0 +1,13 @@
+//
+//  UpgraveViewController.h
+//  eLog
+//
+//  Created by nopilas on 2014-10-08.
+//  Copyright (c) 2014 nopilas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UpgraveViewController : UIViewController
+
+@end
